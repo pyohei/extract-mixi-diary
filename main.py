c4713aee41dc34fc9bf69b34600958c9e124ba50
pyohei <mukaishohei76@gmail.com>
2018-06-21 07:31:02 +0900

    Add parser of argument.

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-"""Extract mixi diary
-
-Created at 2014-07-06.
-"""
+"""Extract and Save mixi diary."""
+import argparse
+import os
 
 import parser
 import extract
@@ -9,10 +8,28 @@ import time
 
 
 def main():
-    diary_ids = parser.parse()
-    for i in diary_ids:
-        extract.get_diary(i)
-        time.sleep(2)
+    """Main script."""
+    args = _parse_argument()
 
+        diary_ids = parser.parse()
+        for i in diary_ids:
+            extract.get_diary(i)
+            time.sleep(2)
+
+
+def _parse_argument():
+    """Parse argument."""
+    p = argparse.ArgumentParser(description='Mixi diary save script.')
+    p.add_argument('--consumer', '-c', help='consumer key', required=True)
+    p.add_argument('--secret', '-s', help='secret key', required=True)
+    p.add_argument('--port', '-p', help='port number of access key', required=True, type=int)
+    p.add_argument('--org', '-o', help='original mixi html dir', required=True)
+    p.add_argument('--dst', '-d', help='output directory of diary', required=True)
+
+    args = p.parse_args()
+    if not os.path.isdir(args.org):
+        print('Your mixi html directory is not found.')
+        quit()
+    return args
 
 main()
