2d11332706bac2b982aa137f3f9a7e9915b754e6
pyohei <mukaishohei76@gmail.com>
2018-06-24 11:57:17 +0900

    Add docstring of parser.py

--- parser.py
+++ parser.py
@@ -1,25 +1,31 @@
-"""Parser of mixi diary list"""
+"""Parser of mixi diary list
+
+This script enable to parse mixi diary id from diary list pages.
+To use this, you execute parse function with file path which having mixi diary id.
+After executing, you can access the list of mixi diary id as `values` variables.
+
+*Attention*
+  This script save mixi diary id as module variables.
+  And `values` variable don't reset in same instance.
+"""
 import codecs
 from html.parser import HTMLParser
 
 values = []
 
 class Mixi(HTMLParser):
-
     
     def handle_starttag(self, tag, attrs):
+        """Parse mixi diary id."""
         if tag == 'input':
             a = dict(attrs)
             if 'class' in a and a['class'] == 'JS_checkbox':
-                if 'value' in a:
+                if 'value' in a and a['value'] not in values:
                     values.append(a['value'])
                 
 
 def parse(file_path):
+    """Parse file."""
     p = Mixi()
     with codecs.open(file_path, 'r', 'utf-8', 'ignore') as f:
         p.feed(f.read())
-    return(values)
-
-if __name__ == '__main__':
-    main()
