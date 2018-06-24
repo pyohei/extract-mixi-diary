34d2cb87a778b33f036dcb141841865a0aadffbc
pyohei <mukaishohei76@gmail.com>
2018-06-24 15:07:24 +0900

    Fix extract logic.

--- extract.py
+++ extract.py
@@ -1,27 +1,48 @@
 """Get and save mixi diary."""
+import os
 import requests
 
-ACCESS_KEY = 'fd44abc91c7356584a5c6116de2060401766c17f'
 
-def get_diary(diary_id):
-    headers = {'Content-Type': 'application/x-www-form-urlencoded'}
-    params = {'access_token': ACCESS_KEY}
-    url = 'http://api.mixi-platform.com/2/diary/articles/@me/@self/{}/'
-    r = requests.get(url.format(str(diary_id)), params=params, headers=headers)
-    print(r)
-    print(r.json())
-    c = r.json()['created']
-    n = _create_file_name(c)
-    print(n)
+class Extractction(object):
 
-    with open(n, 'w') as f:
-        f.write(r.text)
-    print('finish')
+    def __init__(self, port, save_dir):
+        self.port = port
+        self.save_dir = save_dir
 
-def _create_file_name(created):
-    """Create file neme as YYYYmmddHHMMDD.json'"""
-    return '{}.json'.format(
-            created.replace('-', '').replace(':', '').replace('T', '')[0:14])
+    def _get_access_token(self):
+        try:
+            r = requests.get('http://localhost:{}'.format(self.port))
+            return r.text
+        except Exception as e:
+            print('You have trouble to fetch access token(localhost)')
+            print('<-------PythonError------>')
+            print(e)
+            raise
+
+    def save_diary(self, diary_id):
+        access_token = self._get_access_token()
+        headers = {'Content-Type': 'application/x-www-form-urlencoded'}
+        params = {'access_token': access_token}
+        url = 'http://api.mixi-platform.com/2/diary/articles/@me/@self/{}/'
+        r = requests.get(url.format(str(diary_id)), params=params, headers=headers)
+        # Debug
+        print(r)
+        print(r.json())
+        c = r.json()['created']
+        n = self._create_file_name(c)
+        print(n) # Debug
+
+        with open(n, 'w') as f:
+            f.write(r.text)
+
+    def _create_file_name(self, created):
+        """Create file neme as YYYYmmddHHMMDD.json'"""
+        f = '{}.json'.format(
+                created.replace('-', '').replace(':', '').replace('T', '')[0:14])
+        p = os.path.join(self.save_dir, f)
+        return p
     
 if __name__ == '__main__':
-    get_diary(1721036643)
+    # This is test for myself.
+    e = Extractction(9999, '/tmp')
+    e.save_diary(1721036643)
